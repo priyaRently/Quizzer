e9c7c2d34508ff5fe9afeb3b8b4c3119d8aafdf8
Priya Kumari <priyajnv1999@gmail.com>
2023-02-01 11:10:00 +0530

    update

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import Home from './src/screens/Home'
+import Question from './src/screens/Question'
+import Result from './src/screens/Result'
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
